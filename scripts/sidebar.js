cd2fa353b2e9a10ddf3b875ddfc5f7230188198b
Fran-cesca <72857660+Fran-cesca@users.noreply.github.com>
2022-04-11 17:11:40 +0200

    News in scripts and styles

---
+++ scripts/sidebar.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".openbtn").click(function(){
+  $("#mySidebar").css("width", '250px');
+  });
+    });
+
+$(document).ready(function(){
+  $(".closebtn").click(function(){
+  $("#mySidebar").css("width", '0');
+  });
+    });
+
