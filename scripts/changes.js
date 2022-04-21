93e2c23e3405fd7d54cc4f8cb7b4f53c8acfab43
Fran-cesca <72857660+Fran-cesca@users.noreply.github.com>
2022-04-21 12:57:17 +0200

    icone

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,6 +1,6 @@
 /*from fine900 to inizio900*/
 
-$(document).ready(function(){
+/*$(document).ready(function(){
   $("#change").click(function(){
   $("#pagestyle").attr('href','style/inizio900.css');
   $('.title_issue').remove();
@@ -13,21 +13,28 @@ $(document).ready(function(){
   $('#col2 p:first-of-type').replaceWith( "<div id='container'><div id='left'><span class='key' about='colonization'>Colonization</span></div> <div id='center'>rears its ugly head whenever there is</div> <div id='right'><span class='key' about='globalization'> globalization</span></div></div><br>");
   $('#col2 p').wrapAll( "<div id='col2_txt'>");
 
-
-
-
   });
-  });
-/*change style and take care of particularities for each style*/
-function changestyle(name) {
+  });*/
 
-  /*for the bauhaus style, add decorations for the headers of the articles*/
-  if (name == "ourbauhaus.css"){
-    $("div.header").append('<b class="vertical_bar1"></b><b class="bottom_bar2"></b><b class="top_bar3"></b><b class="circle1"></b><b class="square"></b><b class="newbar"></b>');
-  }
-  /*otherwise remove it*/
-  else{
-    $("div.header b:empty").remove();
+function change(name) {
+  if (name == 'belle'){
+  $("#pagestyle").attr('href','style/inizio900.css');
+  $('.title_issue').remove();
+  $('.banner_issue').remove();
+  $('<br>').insertAfter("nav");
+  $('img').remove();
+  $("<img id='ballerini' src='imgs/ballerini.png'/>").insertAfter("#col1 .title");
+  $("<img id='amazzone' src='imgs/amazzone.png'/>").insertAfter("#col2 .title");
+  $("<img id='pattinatrice' src='imgs/pattinatrice.png'/>").insertAfter("#col3 .title");
+  $('#col2 p:first-of-type').replaceWith( "<div id='container'><div id='left'><span class='key' about='colonization'>Colonization</span></div> <div id='center'>rears its ugly head whenever there is</div> <div id='right'><span class='key' about='globalization'> globalization</span></div></div><br>");
+  $('#col2 p').wrapAll( "<div id='col2_txt'>");
+}
+
+if (name == 'tabloid'){
+  
+}
+
+
   };
 
 
