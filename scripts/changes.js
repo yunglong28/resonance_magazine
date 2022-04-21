7f57b9598f19ab6529d1fa42396e040f721d92ba
Alessandro Y. Longo <luneth97@gmail.com>
2022-04-21 14:34:46 +0200

    bo

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,15 +1,42 @@
+/* Pagina scripts per cambiare stili */
+
+
 $(document).ready(function(){
   $("#change").click(function(){
-  $("#pagestyle").attr('href','style/prova.css');
+  $("#pagestyle").attr('href','baroque.css');
   $('img').remove();
+  $('#solar_bar').remove();
   });
   });
+
+
+/* In -> Lowtech style*/
+$(document).ready(function(){
+  $("#changetoLow").click(function (position, text){
+    $("#pagestyle").attr('href','futuristic_typo.css');
+    $('#solar_bar').insertAdjacentHTML(position, text);
+    /*da scrivere come rimuovere tutte le immagini e mettere le altre*/
+  });
+});
 
 $(document).ready(function(){
   $("#three").click(function(){
   $(".home").remove();
   });
   });
+
+
+/*Funzione Lowtech to anotherstyle*/
+$(document).ready(function(){
+  $("#changeLow").click(function(){
+  $("#pagestyle").attr('href','baroque.css');
+  $('#solar_bar').remove();
+  /*da scrivere come rimuovere tutte le immagini e mettere le altre*/
+  });
+  });
+
+
+
 
 
 /*
