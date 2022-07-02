bedaad0bc2b3cef661a1a046646a36f3704a0820
Fran-cesca <72857660+Fran-cesca@users.noreply.github.com>
2022-07-02 18:06:31 +0200

    news

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,22 +1,3 @@
-/* Pagina scripts per cambiare stili */
-/from fine900 to inizio900/
-
-/*$(document).ready(function(){
-  $("#change").click(function(){
-  $("#pagestyle").attr('href','style/inizio900.css');
-  $('.title_issue').remove();
-  $('.banner_issue').remove();
-  $('<br>').insertAfter("nav");
-  $('img').remove();
-  $("<img id='ballerini' src='C:/Users/Francesca/Desktop/balleriniC.png'/>").insertAfter("#col1 .title");
-  $("<img id='ballerini' src='C:/Users/Francesca/Desktop/Vitali/Sito_Vitali/Style/images/amazzone_media.png'>").insertAfter("#col2 .title");
-  $("<img id='pattinatrice' src='C:/Users/Francesca/Desktop/Vitali/Sito_Vitali/Style/images/pattinatrice_piccola.png'/>").insertAfter("#col3 .title");
-  $('#col2 p:first-of-type').replaceWith( "<div id='container'><div id='left'><span class='key' about='colonization'>Colonization</span></div> <div id='center'>rears its ugly head whenever there is</div> <div id='right'><span class='key' about='globalization'> globalization</span></div></div><br>");
-  $('#col2 p').wrapAll( "<div id='col2_txt'>");
-
-  });
-  });*/
-
 function change(name) {
   /*alert(currentstyle)*/
   if (name == 'belle') {
@@ -31,12 +12,19 @@ function change(name) {
     $("#col2 p").wrapAll( "<div id='col2_txt'>");
   }
 
+  else{
+    $("#sep_nav").remove();
+  }
+
   if (name == 'tabloid'){
     $("#pagestyle").attr('href','style/fine900.css');
     $(".container_articles img").remove();
-    $("#sep_nav").remove();
     if($('.title_issue').length == 0){$("<div class='title_issue'> <img src='imgs/theresonance.jpg'> </div> <div class='banner_issue'> EXCLUSIVE: DECOLONISING MUSIC</div>").insertAfter("nav")};
     $("#col1 .title").prepend("<img src='imgs/pallino.png'/>");
+  }
+
+  else {
+    $(".banner_issue").remove();
   }
 
   if (name=='illuminist'){
@@ -48,7 +36,24 @@ function change(name) {
   if (name=='hobbyhorse'){
     $("#pagestyle").attr('href','style/hobbyhorse.css');
     $("img").remove();
+    $("<br>").insertAfter(".navbar");
+    $(".container_articles").attr('class', 'container-fluid container_articles');
+    $("<img src='imgs/folies.jpeg'/>").insertBefore("#title_and_subtitle_1"); 
+    $("<img src='imgs/chocolat.jpeg'/>").insertBefore("#title_and_subtitle_2");
+    $("<img src='imgs/palais.jpeg'/>").insertBefore("#title_and_subtitle_3");
+    $("<img  id='finalfig1' src='imgs/deco1.png' width='300'>").insertAfter("#col1 p:last-of-type");
+    $("<img src='imgs/deco2.png' width='300'>").insertAfter("#col2 p:last-of-type");
+    $("<img src='imgs/deco3.png' width='300'>").insertAfter("#col3 p:last-of-type");
+    $("blockquote").prepend("<img id='block1' src='imgs/birds3.png' width='250'>");
+    /*$(".ArtBody p:first-of-type").each(function(){
+      var first_char = $(this).text().charAt(0);
+      alert(first_char);
+      $(this).text().charAt(0).remove();
+    });*/
+
   }
+
+   /*if (name=='hobbyhorse'){*/
 
 
 };
