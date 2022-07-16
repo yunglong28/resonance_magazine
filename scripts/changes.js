246dace4dbc1106789f960f905cf06a7c8fe9d24
Fran-cesca <72857660+Fran-cesca@users.noreply.github.com>
2022-07-16 11:34:03 +0200

    bla

--- scripts/changes.js
+++ scripts/changes.js
@@ -1,22 +1,60 @@
 function change(name) {
-  /*alert(currentstyle)*/
+  
+  
+
   if (name == 'belle') {
     $("#pagestyle").attr('href','style/inizio900.css');
-    /*$(".container_articles img").remove();*/
-    $("<img class='opening_img' id='ballerini' src='imgs/ballerini.png'/>").insertAfter("#col1 .title");
-    $("<img class='opening_img' id='amazzone' src='imgs/amazzone.png'/>").insertAfter("#col2 .title");
-    $("<img class='opening_img' id='pattinatrice' src='imgs/pattinatrice.png'/>").insertAfter("#col3 .title");
-    /*$("#col2 p:first-of-type").replaceWith( "<div id='container'><div id='left'><span class='key' about='colonization'>Colonization</span></div> <div id='center'>rears its ugly head whenever there is</div> <div id='right'><span class='key' about='globalization'> globalization</span></div></div><br>");*/
-    /*$("#col2 p").wrapAll( "<div id='col2_txt'>");*/
+    if($('#sep_nav').length == 0){$("<br id='sep_nav'>").insertAfter("nav")};
+    $("<img class='opening_img_belle' id='ballerini' src='imgs/ballerini.png'/>").insertAfter("#col1 .title");
+    $("<img class='opening_img_belle' id='amazzone' src='imgs/amazzone.png'/>").insertAfter("#col2 .title");
+    $("<img class='opening_img_belle' id='pattinatrice' src='imgs/pattinatrice.png'/>").insertAfter("#col3 .title");
   }
 
   else{
     $("#container_belle").remove();
+    $(".opening_img_belle").remove();
     /*if($('.op').length == 0){
     $("<img class='op' src='https://img1.wsimg.com/isteam/ip/d8d3ee10-86f5-4a41-aa20-fec4c8ea1c2e/Maracatu%20Coronation.png/:/cr=t:3.65%25,l:0.71%25,w:94.34%25,h:94.34%25/rs=w:1280' alt='Coronation ceremony' width='300'>").insertAfter("#col1 .title");
     $("<img class='op' src='https://2104310a1da50059d9c5-d1823d6f516b5299e7df5375e9cf45d2.ssl.cf2.rackcdn.com/nmbx/2016/11/GaryIngle-475x358.jpg' width='300'>").insertAfter("#col2 .title");
     $("<img class='op'src='https://ia804507.us.archive.org/10/items/pub_popular-music-and-society/pub_popular-music-and-society_itemimage.jpg' width='300'>").insertAfter("#col3 .title");*/
   }
+
+
+
+
+  if (name ==='illuminist'){
+    $("#pagestyle").attr('href','style/illuminism.css');
+    $(".opening_img").remove();
+    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover1.jpeg'>").insertBefore("#col1 .title");
+    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover2.jpeg'>").insertBefore("#col2 .title");
+    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover3.jpeg'>").insertBefore("#col3 .title");
+
+  }
+
+  else {
+    $(".opening_img_illuminism").remove();
+  }
+
+  
+
+  if (name=='hobbyhorse'){
+    $("#pagestyle").attr('href','style/hobbyhorse.css');
+    $("#sep_nav").remove();
+    $(".container_articles").attr('class', 'container-fluid container_articles');
+    $("<img  id='finalfig1' src='imgs/deco1.png' width='300'>").insertAfter("#col1 p:last-of-type");
+    $("<img src='imgs/deco2.png' width='300'>").insertAfter("#col2 p:last-of-type");
+    $("<img src='imgs/deco3.png' width='300'>").insertAfter("#col3 p:last-of-type");
+    if($('.birds').length == 0){$(".quote").prepend("<img class='birds' src='imgs/birds3.png' width='250'>")};
+  }
+
+
+
+  else{
+    $(".container_articles").attr('class', 'container container_articles');
+    $("figcaption").remove();
+    $(".birds").remove();
+  }
+
 
 
   if (name == 'tabloid'){
@@ -32,55 +70,38 @@ function change(name) {
     $("#pallino").remove();
   }
 
-  if (name ==='illuminist'){
-    $("#pagestyle").attr('href','style/illuminism.css');
-    $(".opening_img").remove();
-    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover1.jpeg'>").insertBefore("#col1 .title");
-    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover2.jpeg'>").insertBefore("#col2 .title");
-    $("<img class='img-fluid opening_img opening_img_illuminism' src='imgs/didot_cover3.jpeg'>").insertBefore("#col3 .title");
-
-  }
 
   if (name ==='future'){
     $("#pagestyle").attr('href','style/future.css');
     $(".opening_img").remove();
-    $("<img class='img-fluid article_images' src='imgs/dither_it_fra1.jpg'>").insertAfter("#col1 .title");
-    $("<img class='img-fluid article_images' src='imgs/dither_it_fra2.jpeg'>").insertAfter("#col2 .title");
-    $("<img class='img-fluid article_images' src='imgs/dither_it_fra3.jpeg'>").insertAfter("#col3 .title");
-    $("<div id='solar_bar'><p> This is a solar-powered website, which means it sometimes goes offline. You have 5 minutes of energy left. <img src='imgs/icons8-sun-30.png'><br></p><p>The page size is 86kb. You can print it ecologically <a href='https://www.greenerprinter.com/00'><img src='imgs/icons8-print-30.png'></a><p>Only 0.14 g of CO2 is produced every time someone visits this web page. This is cleaner than 86  % of web pages tested</p><p>Calculate your web page footprint <a href='https://www.websitecarbon.com/'>here</a>.<p/><p>Server Stats <img src='imgs/icons8-solar-panels-32.png'> <br> Location: Barcelona | Time: 16:58 CEST | Battery status: Charging | Power used: 2.50W | Uptime: 12 weeks, 2 days, 6 hours, 19 minutes</p><hr class='half-hr'/></div><br>").insertAfter("#sep_nav");
-    $("<hr>").insertBefore("#sep_nav");
+    if($("#nav_hr").length == 0){
+      $("<hr id='nav_hr'>").insertAfter("nav");
+    }
+    if($("#sep_nav").length == 0){
+      $("<br id='sep_nav'>").insertAfter("#nav_hr");
+    }
+    /*inserire un if per issue*/
+    if($(".opening_img_future").length == 0){
+    $("<img class='img-fluid opening_img_future' src='imgs/dither_it_fra1.jpg'>").insertAfter("#col1 .title");
+    $("<img class='img-fluid opening_img_future' src='imgs/dither_it_fra2.jpeg'>").insertAfter("#col2 .title");
+    $("<img class='img-fluid opening_img_future' src='imgs/dither_it_fra3.jpeg'>").insertAfter("#col3 .title");}
+    if($("#solar_bar").length == 0){$("<div id='solar_bar'><p> This is a solar-powered website, which means it sometimes goes offline. You have 5 minutes of energy left. <img src='imgs/icons8-sun-30.png'><br></p><p>The page size is 86kb. You can print it ecologically <a href='https://www.greenerprinter.com/00'><img src='imgs/icons8-print-30.png'></a><p>Only 0.14 g of CO2 is produced every time someone visits this web page. This is cleaner than 86  % of web pages tested</p><p>Calculate your web page footprint <a href='https://www.websitecarbon.com/'>here</a>.<p/><p>Server Stats <img src='imgs/icons8-solar-panels-32.png'> <br> Location: Barcelona | Time: 16:58 CEST | Battery status: Charging | Power used: 2.50W | Uptime: 12 weeks, 2 days, 6 hours, 19 minutes</p><hr class='half-hr'/></div>").insertAfter("#sep_nav")};
+    $("#cello").attr('src','imgs/dither_it_cello.png');
+    $("#harp").attr('src','imgs/dither_it_harp.png');
+    $("#gramophone").attr('src','imgs/dither_it_gramophone.png');
+    $("#disco").attr('src','imgs/dither_it_disco.png');
+    $("#boombox").attr('src','imgs/dither_it_boombox.png');
+    $("#walkman").attr('src','imgs/dither_it_walkman.png');
+
     /*$("<div class='blackout' id='blackout'><p class='blackout-sentence' id='blackout-sentence'>SERVER IS OUT OF ENERGY</p></div>").insertAfter("head");
     $("<script src='scripts/blackout.js'></script>").insertAfter("#changestyle");*/
   }
 
-
-  if (name=='hobbyhorse'){
-    $("#pagestyle").attr('href','style/hobbyhorse.css');
-    $(".container_articles").attr('class', 'container-fluid container_articles');
-    $("<img  id='finalfig1' src='imgs/deco1.png' width='300'>").insertAfter("#col1 p:last-of-type");
-    $("<img src='imgs/deco2.png' width='300'>").insertAfter("#col2 p:last-of-type");
-    $("<img src='imgs/deco3.png' width='300'>").insertAfter("#col3 p:last-of-type");
-    $(".quote").prepend("<img id='block1' src='imgs/birds3.png' width='250'>");
-    //if ($(".first_char").html() == 'E'){
-        //$(".first_char").replaceWith('<img src="imgs/letterE.png"/>');
-    //};
-
-    /*$(".ArtBody p:first-of-type").each(function(){
-      var first_char = $(this).text().charAt(0);
-      alert(first_char);
-      $(this).text().charAt(0).remove();
-    });*/
-  }
-
-
-
   else{
-    $(".container_articles").attr('class', 'container container_articles');
-    $("figcaption").remove();
+    $("#solar_bar").remove();
+    $("#nav_hr").remove();
+    $(".opening_img_future").remove();
   }
-
-   /*if (name=='hobbyhorse'){*/
-
 
 };
 
