713d4262ab4b97bc8a7317c2d7eff12dac1bff6e
LorenzaPierucci <72924222+LorenzaPierucci@users.noreply.github.com>
2022-08-19 15:20:48 +0200

    document

--- scripts/documentation_script.js
+++ scripts/documentation_script.js
@@ -10,6 +10,14 @@ function change(name) {
 
    if (name == 'hobbyhorse') {
      $("#pagestyle").attr('href','style/documentation_hobbyhorse.css');
+     $(".marquee-section").remove()
+     $("#home_icon").replaceWith("<img class='icons' id='home_icon' src='imgs/magazinetabloid.png'>")
+     $("#cello").replaceWith("<img class='icons' id='cello' src='imgs/cellotabloid.png'>")
+     $("#harp").replaceWith("<img class='icons' id='harp' src='imgs/harptabloid.png'>")
+     $("#gramophone").replaceWith("<img class='icons' id='gramophone' src='imgs/gramophonetabloid.png'>")
+     $("#disco").replaceWith("<img class='icons' id='disco' src='imgs/strobotabloid.png'>")
+     $("#boombox").replaceWith("<img class='icons' id='boombox' src='imgs/boomboxtabloid.png'>")
+     $("#walkman").replaceWith("<img class='icons' id='walkman' src='imgs/ipodtabloid.png'>")
    }
 
 
@@ -19,10 +27,24 @@ function change(name) {
 
     if (name == 'disco') {
      $("#pagestyle").attr('href','style/documentation_analog.css');
+     $("#home_icon").replaceWith("<img class='icons' id='home_icon' src='imgs/magazineanalog.png'>")
+     $("#cello").replaceWith("<img class='icons' id='cello' src='imgs/celloanalog.png'>")
+     $("#harp").replaceWith("<img class='icons' id='harp' src='imgs/harpanalog.png'>")
+     $("#gramophone").replaceWith("<img class='icons' id='gramophone' src='imgs/vynilanalog.png'>")
+     $("#disco").replaceWith("<img class='icons' id='disco' src='imgs/stroboanalog.png'>")
+     $("#boombox").replaceWith("<img class='icons' id='boombox' src='imgs/boomboxanalog.png'>")
+     $("#walkman").replaceWith("<img class='icons' id='walkman' src='imgs/ipodanalog.png'>")
    }
 
    if (name == 'tabloid') {
      $("#pagestyle").attr('href','style/documentation_tabloid.css');
+     $("#home_icon").replaceWith("<img class='icons' id='home_icon' src='imgs/magazinetabloid.png'>")
+     $("#cello").replaceWith("<img class='icons' id='cello' src='imgs/cellotabloid.png'>")
+     $("#harp").replaceWith("<img class='icons' id='harp' src='imgs/harptabloid.png'>")
+     $("#gramophone").replaceWith("<img class='icons' id='gramophone' src='imgs/gramophonetabloid.png'>")
+     $("#disco").replaceWith("<img class='icons' id='disco' src='imgs/strobotabloid.png'>")
+     $("#boombox").replaceWith("<img class='icons' id='boombox' src='imgs/boomboxtabloid.png'>")
+     $("#walkman").replaceWith("<img class='icons' id='walkman' src='imgs/ipodtabloid.png'>")
    }
 
    if (name == 'future') {
@@ -41,7 +63,7 @@ function keepstyle(pagestyle){
 $(document).ready(function(){
   if (sessionStorage.getItem("currentstyle")){
     var style = sessionStorage.getItem("currentstyle");
-    
+
     if (style==="style/home.css"||style==="style/issue.css"){
       change('home')
     }
@@ -75,4 +97,3 @@ $(document).ready(function(){
 
 }
 });
-
